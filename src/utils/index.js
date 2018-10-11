5355805aeaeb171b0150b88e53f6c6113c9485e1
Will <admin@chrisdeMacBook-Pro-2.local>
2018-10-11 11:47:48 +0800

    Update

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@
 export function TimeUpdate(time){
     if (!time) return '';
     let date = new Date(time);
-    let Mouth = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+    let Mouth = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
     let Dates = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
     return date.getFullYear() + '-' + Mouth + '-' + Dates;
 }
